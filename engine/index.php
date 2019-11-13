5633fd7f035d2aa923cb196e73642f65fd0eedc3
Andy Schleising <andy.schleising@fh-erfurt.de>
2019-11-13 15:02:53 +0100

    responsive add

--- engine/index.php
+++ engine/index.php
@@ -3,18 +3,11 @@
     <head>
         <meta charset="UTF-8">
         <meta lang="de">
-
-        <!--Abfrage Ob JS Aktiviert ist oder nichts-->
-    
+        <!-- Responsiv Abfrage -->
+        <link rel="stylesheet" href="assets/css/responsiv.css">
     </head>
 
     <body>
-        <!-- Benutzer wird auf js fähigen seiten geleitet -->
-        <script src="assets/js/loadJSSite.js"></script>
 
-
-
-        <!-- Benutzer wird auf eine rein php/html/css basierte seite geleitet-->
-        sie Benutzen kein js
     </body>
 </html>
