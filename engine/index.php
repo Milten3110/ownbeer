031a44deb0e618100bfef64c08ea3c50ddba800a
Andy Schleising <andy.schleising@fh-erfurt.de>
2019-11-13 15:20:09 +0100

    link menues

--- engine/index.php
+++ engine/index.php
@@ -8,6 +8,19 @@
     </head>
 
     <body>
+        
+    <!-- NavBar -->
+    <div class="navBar">
+        <a href="./" >Home</a>
+        <a href="./pages/anmelden.php" >Events</a>
+        <a href="" >Produkte</a>
+        <a href="" >Warenkorp</a>
+        <a href="" >Kontackt</a>
+        <a href="" >Anmelden</a>
+        <a href="" >Regestrieren</a>
+        <a href="" >Impressum</a>
+    </div>
+    
 
-    </body>
+</body>
 </html>
---
+++ engine/pages/anmelden.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta lang="de">
+</head>
+
+<body>
+    <!-- NavBar , auslagern da für die restlichen Seiten == -->
+    <div class="navBar">
+        <a href="../" >Home</a>
+        <a href="anmelden.php" >Events</a>
+        <a href="produkte.php" >Produkte</a>
+        <a href="warenkorp.php" >Warenkorp</a>
+        <a href="kontackt.php" >Kontackt</a>
+        <a href="anmelden.php" >Anmelden</a>
+        <a href="regestrieren.php" >Regestrieren</a>
+        <a href="impressum.php" >Impressum</a>
+    </div>
+
+</body>
+
+</html>
